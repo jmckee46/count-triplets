c8fd7d5dd2e91fc51284fc488f23f12fc9a87aad
John McKee <jmckee3@mac.com>
2019-11-27 16:04:57 -0800

    working version with logging

--- count-triplets.go
+++ count-triplets.go
@@ -1,3 +1,96 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// {1, 2, 2, 4}
+
+func countTriplets(arr []int64, r int64) int64 {
+	bMap := make(map[int64]int64)
+	cMap := make(map[int64]int64)
+	var count int64
+
+	for _, value := range arr {
+		fmt.Println("value:", value)
+		// check for triplet
+		if num, ok := bMap[value]; ok {
+			count += bMap[value]
+			b := value / r
+			c := b / r
+			fmt.Printf("******%d triplet(s) found: %d, %d, %d\n", num, c, b, value)
+		}
+		// check for double
+		if _, ok := cMap[value]; ok {
+			fmt.Println("double found")
+			bMap[value*r] += cMap[value]
+		}
+		// always add to cMap
+		cMap[value*r]++
+	}
+	fmt.Println("bMap:", bMap)
+	fmt.Println("cMap:", cMap)
+	fmt.Println("count:", count)
+	return count
+}
+
+// test-case-2 output should be:  161700
+// test-case-2 output should be:  166661666700000
+func main() {
+	file, err := os.Open("test-case-2")
+	checkError(err)
+
+	reader := bufio.NewReaderSize(file, 16*1024*1024)
+
+	stdout, err := os.Create("OUTPUT-test-case-2")
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nr := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(nr[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	r, err := strconv.ParseInt(nr[1], 10, 64)
+	checkError(err)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int64
+
+	for i := 0; i < int(n); i++ {
+		arrItem, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arr = append(arr, arrItem)
+	}
+
+	ans := countTriplets(arr, r)
+
+	fmt.Fprintf(writer, "%d\n", ans)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- spec-count-triplets_test.go
+++ spec-count-triplets_test.go
@@ -1,7 +1,41 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestCountTriplets(t *testing.T) {
+func TestCountTriplets1(t *testing.T) {
+	arr := []int64{1, 2, 2, 4}
+	r := int64(2)
 
+	triplets := countTriplets(arr, r)
+
+	if triplets != 2 {
+		t.Errorf("got %d instead of 2", triplets)
+	}
+}
+
+func TestCountTriplets2(t *testing.T) {
+	fmt.Println("")
+	arr := []int64{1, 3, 9, 9, 27, 81}
+	r := int64(3)
+
+	triplets := countTriplets(arr, r)
+
+	if triplets != 6 {
+		t.Errorf("got %d instead of 6", triplets)
+	}
+}
+
+func TestCountTriplets3(t *testing.T) {
+	fmt.Println("")
+	arr := []int64{1, 5, 5, 25, 125}
+	r := int64(5)
+
+	triplets := countTriplets(arr, r)
+
+	if triplets != 4 {
+		t.Errorf("got %d instead of 4", triplets)
+	}
 }
